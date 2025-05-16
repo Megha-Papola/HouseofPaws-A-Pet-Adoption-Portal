a8909ed3fbf1b6dd527f7c6eae97e2a1bf44489f
Megha-Papola <meghapapola05@gmail.com>
2025-05-16 23:51:06 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.mongooseURL, {
   useUnifiedTopology: true,
 })
 .then(() => {
-  console.log('✅ Connected to DB');
+  console.log('Connected to DB');
   const PORT = 4000;
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
