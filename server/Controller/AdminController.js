c4a198d90febf5780a53a63bb078cacae3537a50
Megha-Papola <meghapapola05@gmail.com>
2025-06-04 12:17:14 +0530

    update

--- server/Controller/AdminController.js
+++ server/Controller/AdminController.js
@@ -4,5 +4,5 @@ const getCredentials = (req, res) => {
     const Credentials = { "username": "admin", "password": "1234" }
     res.status(200).json(Credentials)
 }
-
 module.exports = { getCredentials }
+
